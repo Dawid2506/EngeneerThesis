efa79a720d85c029acc200600fe229c71f95fe97
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-03-24 13:32:35 +0100

    idk what i do i do it yesterday

---
+++ blazorApp/BlazorSchedule/backend/company-info/Company.cs
@@ -0,0 +1,11 @@
+namespace BlazorSchedule{
+    public class Company
+    {
+        public string name { get; set; }
+        public string[] workingDays { get; set; }
+        public Dictionary<int, int> workingHours { get; set; }
+        public Dictionary<string, Dictionary<int, int>> workingHoursDay { get; set; }
+        public Dictionary<string, int> helpEmployees { get; set; }
+         
+    }
+}

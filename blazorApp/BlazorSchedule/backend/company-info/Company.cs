a7c1bf254f5ffe684cc0458aee7cdbd83eee5af3
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-03-26 16:44:52 +0100

    company information first page completed

--- blazorApp/BlazorSchedule/backend/company-info/Company.cs
+++ blazorApp/BlazorSchedule/backend/company-info/Company.cs
@@ -3,6 +3,7 @@ namespace BlazorSchedule{
     {
         public string name { get; set; }
         public string[] workingDays { get; set; }
+        public string[] positions { get; set; }
         public Dictionary<int, int> workingHours { get; set; }
         public Dictionary<string, Dictionary<int, int>> workingHoursDay { get; set; }
         public Dictionary<string, int> helpEmployees { get; set; }

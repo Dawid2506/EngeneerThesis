95e9f4a92966b5af69759eadfa9eac23818398ff
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-03-31 14:06:45 +0200

    adjusting the company information component still

--- blazorApp/BlazorSchedule/backend/company-info/Company.cs
+++ blazorApp/BlazorSchedule/backend/company-info/Company.cs
@@ -1,12 +1,36 @@
 namespace BlazorSchedule{
     public class Company
     {
+        private static Company instance;
+        private static readonly object lockObject = new object();
+
         public string name { get; set; }
-        public string[] workingDays { get; set; }
-        public string[] positions { get; set; }
+        public List<string> workingDays { get; set; }
+        public List<string> positions { get; set; }
         public Dictionary<int, int> workingHours { get; set; }
         public Dictionary<string, Dictionary<int, int>> workingHoursDay { get; set; }
-        public Dictionary<string, int> helpEmployees { get; set; }
-         
+
+        private Company()
+        {
+            // Private constructor to prevent instantiation
+        }
+
+        public static Company Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Company();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
     }
 }

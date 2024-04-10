1978827444ca57f311e1248b972131b4237b9da6
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-04-10 22:51:14 +0200

    offDays data

--- blazorApp/BlazorSchedule/backend/employee-info/Employee.cs
+++ blazorApp/BlazorSchedule/backend/employee-info/Employee.cs
@@ -4,6 +4,9 @@ namespace BlazorSchedule{
         public string name { get; set; }
         public string typeOfAgreement { get; set; }
         public int minHours { get; set; }
+        public List<int> daysOff { get; set; }
+        public int SelectedDayOff { get; set; }
+        public bool[,] personalSchedule { get; set; }
 
         public Employee(string name, string typeOfAgreement, int minHours)
         {

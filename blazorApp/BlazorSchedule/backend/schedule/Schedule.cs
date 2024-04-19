b00f3f0f0fe1f0ce9de17e3cdfcf2aa45d737a21
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-04-19 16:18:14 +0200

    first (zero) layer of my schedule working corectly

--- blazorApp/BlazorSchedule/backend/schedule/Schedule.cs
+++ blazorApp/BlazorSchedule/backend/schedule/Schedule.cs
@@ -27,9 +27,38 @@ public class Schedule
 
     public bool[,] schedule { get; set; }
 
-    public void InitializeSchedule(int numberOfDays)
+    public void InitializeSchedule(int numberOfDays, List<int> workingDaysInt, int firstDayOfMonth)
     {
         schedule = new bool[numberOfDays,5];
-        Console.WriteLine(schedule.GetLength(0));
+        //Console.WriteLine(schedule.GetLength(0));
+        
+        int actualDay = firstDayOfMonth;
+        for(int i = 0; i < numberOfDays; i++)
+        {
+            for(int j = 0; j < workingDaysInt.Count; j++)
+            {
+                if(workingDaysInt[j] == actualDay)
+                {
+                    schedule[i,0] = true;
+                }
+            }
+            if(actualDay == 6)
+            {
+                actualDay = 0;
+            }
+            else
+            {
+                actualDay++;
+            }
+        }
+
+        
+    }
+
+    public void PrintZeroLayerOfSchedule(){
+        for(int i = 0; i < schedule.GetLength(0); i++)
+        {
+            Console.WriteLine(schedule[i,0]);
+        }
     }
 }

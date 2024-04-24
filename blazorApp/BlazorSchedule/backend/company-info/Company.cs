49f980e6ddc81a64cbec6d9f360dda0c48bbad11
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-04-24 18:56:33 +0200

    saving data about working hours working correctly

--- blazorApp/BlazorSchedule/backend/company-info/Company.cs
+++ blazorApp/BlazorSchedule/backend/company-info/Company.cs
@@ -4,11 +4,12 @@ namespace BlazorSchedule{
         private static Company instance;
         private static readonly object lockObject = new object();
 
-        public string name { get; set; }//
-        public List<string> workingDays { get; set; }//
+        public string name { get; set; }
+        public List<string> workingDays { get; set; }
         public Dictionary<string, Dictionary<string, string>> workingHoursDay { get; set; }
         public Dictionary<string, List<string>> positionsPerDay { get; set; }
-        public List<string> positionsList { get; set; }//
+        public List<string> positionsList { get; set; }
+        public bool isCheckboxChecked { get; set; } = false;
 
         private Company()
         {

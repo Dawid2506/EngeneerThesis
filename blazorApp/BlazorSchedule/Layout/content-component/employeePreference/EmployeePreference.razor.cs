b6e7ef08d92c5440afa5cafd8addd9eaef05a8aa
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-09-09 00:38:59 +0200

    refactor(EmployeePreference): refactor finished

---
+++ blazorApp/BlazorSchedule/Layout/content-component/employeePreference/EmployeePreference.razor.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchedule.Layout.content_component.employeePreference
+{
+    public partial class EmployeePreference
+    {
+        private string? errorMessage { get; set; }
+        private List<Employee> employees { get; set; } = new List<Employee>();
+        private IEmployeesRepository? employeesRepository { get; set; }
+
+        protected override void OnInitialized()
+        {
+            employeesRepository = appState.EmployeesRepository;
+            employees = employeesRepository.GetEmployees();
+        }
+
+        private void AddDate(Employee employee)
+        {
+            if (employee.SelectedDayOff <= 0 || employee.SelectedDayOff >= 32)
+            {
+            errorMessage = "Invalid date format";
+            return;
+            }
+
+            errorMessage = null;
+            employee.daysOff.Add(employee.SelectedDayOff);
+        }
+
+        private void SubtractDay(Employee employee, int index)
+        {
+            if (employee.daysOff == null || index < 0 || index >= employee.daysOff.Count)
+            {
+            return;
+            }
+
+            employee.daysOff.RemoveAt(index);
+        }
+    }
+}

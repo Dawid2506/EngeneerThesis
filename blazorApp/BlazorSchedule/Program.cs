9f006de1daa1844b999e23a02fab41c0d4fe59b0
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-10-16 18:12:09 +0200

    feat(blazorise): Blazorise has been configured

--- blazorApp/BlazorSchedule/Program.cs
+++ blazorApp/BlazorSchedule/Program.cs
@@ -1,13 +1,23 @@
 using BlazorSchedule;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddHttpClient();
-builder.Services.AddBlazorBootstrap();
 builder.Services.AddSingleton<AppState>();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
